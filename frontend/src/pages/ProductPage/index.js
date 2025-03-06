b1eab4c3e9aa7128ca197498816b38ba200c89a6
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-03-06 18:33:47 +0100

    cart ui is done

--- frontend/src/pages/ProductPage/index.js
+++ frontend/src/pages/ProductPage/index.js
@@ -47,8 +47,27 @@ const ProductPage = () => {
             <Col sm={5} md={5} lg={5} className="card-cnt">
             <Card.Img variant="top" src={`http://localhost:3000/api/product/uploads/${product.image}`} />
             </Col>
-            <Col sm={5} md={5} lg={5}>
-                <h4>test</h4>
+            <Col sm={5} md={5} lg={5} className="prod-desc">
+                <h1>{product.title}</h1>
+                <h4>{product.price}</h4>
+                <p>{product.description}</p>
+                  <Col sm={12} md={12} lg={12}>
+                      <Row>
+                          <Col sm={2} md={2} lg={1} className="p-0 c-b">
+                              <Button variant="dark">-</Button>
+                          </Col>
+                          <Col sm={2} md={2} lg={1} className="c-b">
+                              <span>1</span>
+                          </Col>
+                          <Col sm={2} md={2} lg={1} className="p-0 c-b">
+                              <Button variant="dark">+</Button>
+                          </Col>
+
+                          <Col sm={2} md={2} lg={3} className="p-0 m-l-50 cart-btn">
+                              <Button variant="dark">Add to cart</Button>
+                          </Col>
+                      </Row>
+                  </Col>
             </Col>
         </Row>
     }
