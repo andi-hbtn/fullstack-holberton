faa45c801c4a856eb3c6dd1fb9a08bbf2783e5d0
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-03-07 17:49:54 +0100

    cart

--- frontend/src/pages/ProductPage/index.js
+++ frontend/src/pages/ProductPage/index.js
@@ -10,8 +10,39 @@ const ProductPage = () => {
   const { id } = useParams();
   const { getProduct } = useProductContext();
   const [product, setProduct] = useState([]);
-  const [error, setError] = useState({message:"",status:0});
+  const [cart, setCart] = useState(
+        {
+            user_id:null,
+            items:[],
+            total_price:0,
+            status:"pending",
+            createdAt:new Date()
+        }
+    );
 
+    const handleIncrement = (e) =>{
+       setCart((prevState)=>{
+
+        
+
+        const newItem = [];
+        newItem.push({product_id:product.id,quantity:1});
+        prevState.items.push(newItem);
+
+       prevState.items.reduce((total,item)=>{
+        console.log("item----",item);
+        console.log("total----",total);
+        });
+
+        //console.log("newTotal----",newTotal);
+
+        
+
+        return{...prevState}
+       })
+      }
+
+  const [error, setError] = useState({message:"",status:0});
 
   useEffect(()=>{
     getById(id)
@@ -28,7 +59,14 @@ const ProductPage = () => {
     }catch(error){
         console.log(error);
     }
-  } 
+  }
+
+  const handleDecrement = (e) =>{
+
+  }
+
+ 
+
 
   return (
     <>
@@ -54,13 +92,13 @@ const ProductPage = () => {
                   <Col sm={12} md={12} lg={12}>
                       <Row>
                           <Col sm={2} md={2} lg={1} className="p-0 c-b">
-                              <Button variant="dark">-</Button>
+                              <Button variant="dark" onClick={handleDecrement}>-</Button>
                           </Col>
                           <Col sm={2} md={2} lg={1} className="c-b">
-                              <span>1</span>
+                              <span>{cart.total_price}</span>
                           </Col>
                           <Col sm={2} md={2} lg={1} className="p-0 c-b">
-                              <Button variant="dark">+</Button>
+                              <Button variant="dark" onClick={handleIncrement}>+</Button>
                           </Col>
 
                           <Col sm={2} md={2} lg={3} className="p-0 m-l-50 cart-btn">
