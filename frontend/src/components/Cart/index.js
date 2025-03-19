f4fc19d55af6db4763e23b155a6980fef7f0c3d5
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-03-19 17:28:49 +0100

    cart page and products are displayed

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import Header from "../Header/Header";
-import { Container,Row,Col } from "react-bootstrap";
+import { Container,Row,Col,Table,Button} from "react-bootstrap";
 import { useCartContext } from "../../context/CartContext";
+import "./index.css";
 
 const Cart = () =>{
 
@@ -10,55 +11,96 @@ const Cart = () =>{
         const [cart,setCart] = useState([]);
         useEffect(() => {
             const storedCart = JSON.parse(localStorage.getItem("items")) || [];
-            const items = storedCart.map((el,index)=>{
-                console.log("tmp----",el);
-                return {
-                    product_id: el.items[0].product_id, 
-                    title: el.items[0].title, 
-                    image: el.items[0].image, 
-                    price: 10, quantity: el.items.length
-                };
-            });
+            loadItems(storedCart);
+        },[]);
 
-           
-        }, []);
+        const loadItems = (items)=>{
+            const result = items.map((el,index)=>{
+               return {
+                   product_id: el.items[0].product_id,
+                   title: el.items[0].title,
+                   image: el.items[0].image,
+                   price: 10, quantity: el.items.length
+               };
+           });
+           setCart(result);
+        }
 
-        //console.log("cart----",cart);
 
-    return(
-        <>
-        <Header/>
-            <Container>
-                <Row className="prod-cart-cnt">
-                    <Col>
-                        <h4>Cart</h4>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={12} md={8} lg={8}>
-                    {/* {
-                        cart.map((item,index)=>{
-                            return(
-                                <Row>
-                                    <Col sm={6} md={6} lg={6}>Product</Col>
-                                    <Col sm={3} md={3} lg={3}>Product</Col>
-                                    <Col sm={3} md={3} lg={3}>Product</Col>
-                                </Row>
-                            )
-                        })
-                    } */}
-                    </Col>
-                    <Col sm={12} md={4} lg={4}>
-                        <Row>
-                            <Col sm={12} md={12} lg={12}>
-                                <h4>Cart totals</h4>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+        // console.log("cart-----",cart);
+
+        return(
+            <>
+            <Header/>
+                <Container>
+                    <Row className="prod-cart-cnt">
+                        <Col>
+                            <h4>Cart</h4>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={12} md={8} lg={8}>
+                        <Table>
+                            <thead>
+                                <tr>
+                                <th>Product</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cart.map((item,index)=>{
+                                        console.log("item----",item);
+                                        return(
+                                        
+                                            <tr>
+                                                <td>
+                                                <div className='item-img-cnt'>
+                                                        <img src={`http://localhost:3000/api/product/uploads/${item.image}`} height={"150px"} width={"150"} alt='product name'/>
+                                                </div>
+                                                <div className='item-desc-cnt'>
+                                                        <a href=''>{item.title}</a>
+                                                        <p>
+                                                            <span>${item.price}</span>
+                                                        </p>
+                                                </div>
+                                                </td>
+                                                <td>
+                                                   <Row>
+                                                    <Col sm={2} md={2} lg={1} className="p-0 c-b">
+                                                            <Button variant="dark">-</Button>
+                                                        </Col>
+                                                        <Col sm={2} md={2} lg={1} className="c-b">
+                                                            <span>{item.quantity}</span>
+                                                        </Col>
+                                                        <Col sm={2} md={2} lg={1} className="p-0 c-b">
+                                                            <Button variant="dark">+</Button>
+                                                        </Col>
+                                                   </Row>
+                                                </td>
+                                                <td>
+                                                   <span>$ {item.price * item.quantity}</span>
+                                                </td>
+                                            </tr>
+                                            
+                                        )
+                                    })
+                                }
+                            </tbody>
+                         </Table>
+                        </Col>
+                        <Col sm={12} md={4} lg={4}>
+                            <Row>
+                                <Col sm={12} md={12} lg={12}>
+                                    <h4>Cart totals</h4>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
 }
 
 export default Cart;
