b29cedff0297241b126b2842ed9e85532d211720
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-04-15 18:56:55 +0200

    bug fix for user in case token has expired

--- frontend/src/components/AdminRoute/index.js
+++ frontend/src/components/AdminRoute/index.js
@@ -5,28 +5,21 @@ import { useEffect, useState } from "react";
 
 const AdminRoute = ({ children }) => {
 	const { authUser } = useAuthenticateContext();
-    const [hasAccess,setHasAccess] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [hasAccess, setHasAccess] = useState(false);
 
-    useEffect(()=> {
-        console.log("authUser--in--useEffect--",authUser);
-        if (authUser) {
-            if (authUser?.roles === 'admin') {
-                setHasAccess(true);
-            } else {
-                setHasAccess(false);
-            }
-        }
-    }, [authUser]);
+useEffect(() => {
+    console.log("authUser----in----useEffect-",authUser);
+    
+	if (authUser !== null) {
+		setHasAccess(authUser?.roles === 'admin');
+		setLoading(false);
+	}
+}, [authUser]);
 
-    console.log("hasAccess----",hasAccess);
-
-    if (hasAccess === null) {
-        return <Navigate to="/" />
-    } else if (hasAccess) {
-        return children
-    } else {
-        return <Navigate to="/" />
-    }
-};
-
+ if (loading) {
+	return <div>Loading...</div>; // or a spinner
+}
+    return hasAccess ? children : <Navigate to="/" />;
+}
 export default AdminRoute;
