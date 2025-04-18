071f52b2aaf52c128aec5912adac268b3b3eaf7a
devis <devis@paperclicks.net>
2025-04-18 11:54:43 +0200

    updated url for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://london-glass-fitting-frontend.fyirir.easypanel.host/",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: "https://london-glass-fitting-frontend.fyirir.easypanel.host",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", 'OPTIONS'],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
