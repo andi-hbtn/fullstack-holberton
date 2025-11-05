66d765c05f638b89890f32b90ed34b6635d18abd
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-11-05 18:34:54 +0100

    only 5 items per row and they have lot of space

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -20,32 +20,31 @@ const Home = () => {
 			<Header onSearchChange={setSearchResults} />
 			<div className="luxe-glass-home">
 				<Slider />
-				<Container className="categories-section">
-					<Row className="justify-content-center">
-						{resultsToShow.map((item, index) => (
-							<Col key={index} lg={4} md={6} className="mb-5 card-link-contaier">
-								<Link to={item?.products ? `/category/${item.id}` : `/product/${item.id}`} className="card-link">
-									<Card className="luxe-card">
-										<div className="card-image-container">
-											<Card.Img
-												variant="top"
-												src={`${process.env.REACT_APP_API_URL}${item.price ? 'api/product/uploads/' : 'api/category/uploads/'}${item.image}`}
-												className="card-image"
-												alt={item.title}
-											/>
-											<div className="image-overlay"></div>
-										</div>
-										<Card.Body className="text-center category-card">
-											<Card.Title>
-												<h3>{item.title}</h3>
-											</Card.Title>
-										</Card.Body>
-									</Card>
-								</Link>
-							</Col>
-						))}
-					</Row>
-				</Container>
+				<Row className="justify-content-evenly">
+					{resultsToShow.map((item, index) => (
+						<Col key={index} lg={4} md={6} className="mb-5 card-link-contaier">
+							<Link to={item?.products ? `/category/${item.id}` : `/product/${item.id}`} className="card-link">
+								<Card className="luxe-card">
+									<div className="card-image-container">
+										<Card.Img
+											variant="top"
+											src={`${process.env.REACT_APP_API_URL}${item.price ? 'api/product/uploads/' : 'api/category/uploads/'}${item.image}`}
+											className="card-image"
+											alt={item.title}
+										/>
+										<div className="image-overlay"></div>
+									</div>
+									<Card.Body className="text-center category-card">
+										<Card.Title>
+											<h3>{item.title}</h3>
+										</Card.Title>
+									</Card.Body>
+								</Card>
+							</Link>
+						</Col>
+					))}
+				</Row>
+
 			</div>
 			<Footer />
 		</>
