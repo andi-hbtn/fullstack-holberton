022aabbec2ffcdc7975708cb316d66e15349e9e5
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-11-10 15:49:22 +0100

    home page changes for category

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -20,31 +20,32 @@ const Home = () => {
 			<Header onSearchChange={setSearchResults} />
 			<div className="luxe-glass-home">
 				<Slider />
-				<Row className="justify-content-evenly g-0">
-					{resultsToShow.map((item, index) => (
-						<Col key={index} lg={4} md={6} className="mb-5 card-link-contaier">
-							<Link to={item?.products ? `/category/${item.id}` : `/product/${item.id}`} className="card-link">
-								<Card className="luxe-card">
-									<div className="card-image-container">
-										<Card.Img
-											variant="top"
-											src={`${process.env.REACT_APP_API_URL}${item.price ? 'api/product/uploads/' : 'api/category/uploads/'}${item.image}`}
-											className="card-image"
-											alt={item.title}
-										/>
-										<div className="image-overlay"></div>
-									</div>
-									<Card.Body className="text-center category-card">
-										<Card.Title>
-											<h3>{item.title}</h3>
-										</Card.Title>
-									</Card.Body>
-								</Card>
-							</Link>
-						</Col>
-					))}
-				</Row>
-
+				<Container className="category-container">
+					<Row>
+						{resultsToShow.map((item, index) => (
+							<Col key={index} lg={4} md={6} className="mb-5 card-link-contaier">
+								<Link to={item?.products ? `/category/${item.id}` : `/product/${item.id}`} className="card-link">
+									<Card className="luxe-card">
+										<div>
+											<Card.Img
+												variant="top"
+												src={`${process.env.REACT_APP_API_URL}${item.price ? 'api/product/uploads/' : 'api/category/uploads/'}${item.image}`}
+												className="card-image"
+												alt={item.title}
+											/>
+											<div className="image-overlay"></div>
+										</div>
+										<Card.Body className="text-center category-card">
+											<Card.Title>
+												<h3>{item.title}</h3>
+											</Card.Title>
+										</Card.Body>
+									</Card>
+								</Link>
+							</Col>
+						))}
+					</Row>
+				</Container>
 			</div>
 			<Footer />
 		</>
